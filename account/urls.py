aecc0cef286f181dd327d30e4a27f52b4ce26fb2
antonnifo <antonnifo@live.com>
2021-07-22 14:03:31 +0300

    implement login and password reset functionality

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    # post views
+        path('', include('django.contrib.auth.urls')), 
+]
--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
 ]
 
 if settings.DEBUG:
