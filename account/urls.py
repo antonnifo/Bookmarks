16bbb813b6c5d75ae3475de76d9b7941e6b5f775
antonnifo <antonnifo@live.com>
2021-07-24 08:14:10 +0300

    register users

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # post views
         path('', include('django.contrib.auth.urls')), 
+        path('register/', views.register, name='register'),
 ]
