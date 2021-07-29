e84ec59c7dbe16cb27a99ac89a187a9134dc993f
antonnifo <antonnifo@live.com>
2021-07-29 17:44:14 +0300

    add model forms to manage user profile

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from .models import Profile
 from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
@@ -20,4 +20,14 @@ class UserRegistrationForm(forms.ModelForm):
             cd = self.cleaned_data
             if cd['password'] != cd['password2']:
                 raise forms.ValidationError('Passwords don\'t match.')
-                return cd['password2']
+            return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')            
