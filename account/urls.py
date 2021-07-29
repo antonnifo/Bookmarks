a0c21b6799ee8574add3adea2683ac7600b9e0dc
antonnifo <antonnifo@live.com>
2021-07-29 17:46:38 +0300

    map profile edit view

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # post views
+        path('', views.dashboard, name='dashboard'),
         path('', include('django.contrib.auth.urls')), 
         path('register/', views.register, name='register'),
         path('edit/', views.edit, name='edit'),
