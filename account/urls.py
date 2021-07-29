ba9e0e0dd8d1bf253540fb53155ca1808b20aa3e
antonnifo <antonnifo@live.com>
2021-07-29 17:27:40 +0300

    add path to edit user details

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # post views
         path('', include('django.contrib.auth.urls')), 
         path('register/', views.register, name='register'),
+        path('edit/', views.edit, name='edit'),
 ]
