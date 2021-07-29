3bd7586bd0ab3323aeb6d62fc95dff55ce721f4f
antonnifo <antonnifo@live.com>
2021-07-29 17:47:29 +0300

    add view to edit profile

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
-from .forms import  UserRegistrationForm
+
+from .forms import  UserRegistrationForm, \
+              UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 @login_required
@@ -23,6 +24,8 @@ def register(request):
                             user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
             return render(request,
                     'account/register_done.html',
                     {'new_user': new_user})                                   
@@ -32,3 +35,28 @@ def register(request):
         return render(request,
                     'account/register.html',
                     {'user_form': user_form})                
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                        data=request.POST)
+
+        profile_form = ProfileEditForm(
+                        instance=request.user.profile,
+                        data=request.POST,
+                        files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+                user_form.save()
+                profile_form.save()                
+
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+                            instance=request.user.profile)
+
+    return render(request,
+                        'account/edit.html',
+                        {'user_form': user_form,
+                        'profile_form': profile_form})                        
