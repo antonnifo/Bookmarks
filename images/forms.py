0dfc8bcf5213b20a07e6033afa2a3bfbe234d2ae
antonnifo <antonnifo@live.com>
2021-07-31 19:41:52 +0300

    download the image before saving the form in db

--- images/forms.py
+++ images/forms.py
@@ -1,5 +1,11 @@
+from urllib import request
+
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
 from .models import Image
+
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
@@ -17,3 +23,19 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError('The given URL does not ' \
                                 'match valid image extensions.')
         return url    
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+       
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+
+        if commit:
+            image.save()
+        return image
