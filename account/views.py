9ed6de8cebfbfbc54168528422f854e7375c71bc
antonnifo <antonnifo@live.com>
2021-08-04 19:21:42 +0300

    create list and detail view for user profile

--- account/views.py
+++ account/views.py
@@ -5,6 +5,9 @@ from .forms import  UserRegistrationForm, \
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
 
 @login_required
 def dashboard(request):
@@ -66,3 +69,19 @@ def edit(request):
                         'account/edit.html',
                         {'user_form': user_form,
                         'profile_form': profile_form})                        
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,
+                'account/user/list.html',
+                {'section': 'people',
+                'users': users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(request,
+                'account/user/detail.html',
+                {'section': 'people',
+                'user': user})                        
